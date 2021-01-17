6671b9983ab6558e6eb747d08ee7a53338ef5dfa
dmjau <dmjaume.bsas@gmail.com>
2021-01-17 12:11:17 -0300

    added method to update company name on the company services

---
+++ src/components/EditHeaderName.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { companyService } from '../services/company.services';
+
+const EditHeaderName = () => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    companyService
+      .getData()
+      .then((res) => {
+        setData(res.data.name);
+      })
+      .catch((err) => {
+        console.log('Unexpected Error', err);
+      });
+  };
+
+  return (
+    <div>
+      <div className="flex-large">
+        <h5>{data}</h5>
+      </div>
+    </div>
+  );
+};
+export default EditHeaderName;
--- src/services/company.services.js
+++ src/services/company.services.js
@@ -3,6 +3,7 @@ import API from './config.services';
 // create an Object to send the URI to access
 const COMPANY_ENDPOINT = {
   DATA: '/companies' // URI to get the basic data
+  EDIT: '/companies/' // URI to edit company name
 };
 
 export const companyService = {
@@ -10,6 +11,17 @@ export const companyService = {
   getData: () =>
     new Promise((resolve, reject) => {
       API.get(COMPANY_ENDPOINT.DATA)
+        .then((res) => {
+          resolve(res.data);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    }),
+  // Function to edit a company by id
+  updateById: (id, data) =>
+    new Promise((resolve, reject) => {
+      API.patch(COMPANY_ENDPOINT.EDIT + id, data)
         .then((res) => {
           resolve(res.data);
         })
